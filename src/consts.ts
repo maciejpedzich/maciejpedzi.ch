0e6e2138934622193f7e36f2701b8e1e6d191f1c
Maciej Pędzich <dnalop.me@gmail.com>
2023-05-02 08:23:37 +0200

    Use lowercase text for branding consistency

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Maciej Pędzich';
-export const SITE_DESCRIPTION = 'Vue.js/Nuxt/Alpine/Astro Developer';
+export const SITE_TITLE = 'maciej pędzich';
+export const SITE_DESCRIPTION =
+  'frontend developer and motorsport fan in one person';
