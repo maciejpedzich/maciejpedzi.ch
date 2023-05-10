666e50d317ec4c3fa04e8a60a5509a270ee7ba43
Maciej Pędzich <dnalop.me@gmail.com>
2023-05-10 22:42:15 +0200

    Remove pipe between first and last name in title

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'maciej | pędzich';
+export const SITE_TITLE = 'maciej pędzich';
 export const SITE_DESCRIPTION =
   'frontend developer and motorsport fan in one person';
