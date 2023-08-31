387479e05d78e8adb27eafb35fbf8b05e6c7e75c
Maciej Pędzich <contact@maciejpedzi.ch>
2023-08-31 15:50:20 +0200

    Migrate to Astro 3.0

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,11 +2,12 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
-export async function get(context) {
+export async function GET(context) {
   const posts = (await getCollection('blog')).sort(
     (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
   );
-  return rss({
+
+  const { body } = await rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
@@ -14,5 +15,12 @@ export async function get(context) {
       ...post.data,
       link: `/blog/${post.slug}/`
     }))
+  });
+
+  return new Response(body, {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/rss+xml'
+    }
   });
 }
