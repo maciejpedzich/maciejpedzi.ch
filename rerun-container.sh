522f91f12ad75ea9ab34f0abd782c46091fa2e56
Maciej Pędzich <contact@maciejpedzi.ch>
2024-03-26 22:43:51 +0100

    Ensure container ID gets properly obtained before removal

--- rerun-container.sh
+++ rerun-container.sh
@@ -1,2 +1,3 @@
-docker container rm -f $(docker ps -aqf "ancestor=maciejpedzi.ch:latest")
+docker stop my_website && \
+docker container rm $(docker ps -aqf "name=my_website") && \
 docker run -d -p 8505:80 --name my_website maciejpedzi.ch:latest
