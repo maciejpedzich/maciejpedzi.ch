454e2e82748636492b318297a720e90960b7bb06
Maciej Pędzich <contact@maciejpedzi.ch>
2024-04-06 11:33:32 +0200

    Pass action env variables as script args

--- rerun-container.sh
+++ rerun-container.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/sh
 
+CONTAINER_NAME = $1;
+PORT_PAIR = $2;
+NETWORK_NAME = $3;
+IMAGE_TAG = $4;
+
 docker stop $CONTAINER_NAME && \
 docker container rm $(docker ps -aqf "name=$CONTAINER_NAME") && \
 docker run -d -p $PORT_PAIR --netwok $NETWORK_NAME --name $CONTAINER_NAME $IMAGE_TAG
