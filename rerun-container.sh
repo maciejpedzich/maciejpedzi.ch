6c0d28f6aa35732736175d27058d0e33e866ef2f
Maciej Pędzich <contact@maciejpedzi.ch>
2024-07-16 09:24:53 +0200

    Remove redundant posts and scripts

--- rerun-container.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/sh
-
-docker stop my_website && \
-docker container rm $(docker ps -aqf "name=my_website") && \
-docker run -d -p 8505:80 --network=my_network --name my_website maciejpedzi.ch:latest
